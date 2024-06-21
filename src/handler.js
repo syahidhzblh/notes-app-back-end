5d8b4c1a93692af07371e0eb31b06427e6b189cd
syahidhzblh <syahidhizbullah17@gmail.com>
2024-06-21 14:44:43 +0700

    Update: CRUD Handler

--- src/handler.js
+++ src/handler.js
@@ -34,4 +34,73 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal menampilkan catatan',
+  });
+  response.code(500);
+  return response;
+};
+
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+  const updateAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updateAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan Berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal mengubah data catatan',
+  });
+  response.code(500);
+  return response;
+};
+
+const deleteNoteByIdHandler = (request, id) => {
+  const { id } = request.params;
+
+}
+
+module.exports = {
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+};
